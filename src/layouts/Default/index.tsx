bf674c7f5d5451fdf44de365b7bf890a73c9c0b4
goiliace <zombizombi51@gmail.com>
2024-04-22 01:32:18 +0700

    ok

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,8 +1,20 @@
 import { ReactElement } from 'react'
 import Nav from '~/components/Nav';
 import ChatWidget from '~/components/ChatWidget';
+import { useAppDispatch, useAppSelector } from '~/store'
+import { validateUser } from '~/features/Auth/thunks'
+
 const DefaultLayout = ({ children }: { children: ReactElement }) => {
     const pageTypes = ['Login', 'SignUp']
+    const user = useAppSelector(state => state?.auth?.user)
+    const dispatch = useAppDispatch()
+    if (!user) {
+        // get from cookie
+        const token = localStorage.getItem('token');
+        if (token) {
+            dispatch(validateUser(token));
+        }
+    }
     return (
         <div className="flex flex-col min-h-screen">
             <div className={`relative flex flex-grow ${!pageTypes.some(pageType => children.type.toString().includes(pageType)) && 'space-y-[72px]'}`}>
